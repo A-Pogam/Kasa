0d8cf13557fab156ed8e50e10cb6580a5e8f444d
A-Pogam <alicepogam59@hotmail.com>
2023-06-01 11:26:21 +0200

    Improvement html (min) and organisation folders/files

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,5 @@
-// import logo from './logo.svg';
-// import './App.css';
 import React from 'react';
+import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function Home() {
