f405ab4da913e3c8f30c369d8e78902f5eb4b678
A-Pogam <alicepogam59@hotmail.com>
2023-06-02 15:08:20 +0200

    improvement index code

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
- 
-import About from "./pages/about"
-import Error from "./pages/error"
-import Home from "./pages/home"
-import Housing from "./pages/housing"
 
-//import Header from './components/header'
+import About from './pages/about'
+import Error from './pages/error'
+import Home from './pages/home'
+import Housing from './pages/housing'
 
-
-  ReactDOM.render(
-    <React.StrictMode>
-        <Router>
-            <Routes>
-              <Route path="/about" element={<About />} />
-              <Route path="*" element={<Error />} />
-              <Route path="/" element={<Home />} />
-              <Route path="/housing/:id" element={<Housing />} />
-            </Routes>
-            <Home/>
-        </Router>
-    </React.StrictMode>,
-document.getElementById('root')
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/housing/:id" element={<Housing />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 )
