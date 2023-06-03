d6b228dc7c340d96784cea9a990b5e256a770d5b
A-Pogam <alicepogam59@hotmail.com>
2023-06-03 21:42:22 +0200

    created housingtags props

---
+++ src/components/housingTags.jsx
@@ -0,0 +1,9 @@
+const HousingTags = (props) => {
+    return (
+            <ul>
+                <li className="tags">{props.tags}</li>
+            </ul>
+    );
+  };
+  
+  export default HousingTags;
