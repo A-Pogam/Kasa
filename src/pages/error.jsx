a52e8af0d6d0ac3a70cfddb76705364c45326ea4
A-Pogam <alicepogam59@hotmail.com>
2023-06-03 20:20:28 +0200

    Created link back error at error page

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const Error = () => {
   return (
     <div>
             <h1>Oups ! La page que vous demandez n'existe pas</h1>
+      <ul>
+            <li>
+              <Link className="back-home" to="/">
+                Retourner sur la page d'acceuil
+              </Link>
+            </li>
+      </ul>
     </div>
   );
 };
