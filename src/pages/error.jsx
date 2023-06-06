46a4bdeedebdad3c037cde51668b81b4a143ead1
A-Pogam <alicepogam59@hotmail.com>
2023-06-06 18:13:06 +0200

    imporvement error page

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 const Error = () => {
   return (
     <div>
-            <h1>Oups ! La page que vous demandez n'existe pas</h1>
+            <h1>404</h1>
+            <p>Oups ! La page que vous demandez n'existe pas</p>
       <ul>
             <li>
               <Link className="back-home" to="/">
