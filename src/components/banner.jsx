cb76877bb14fcc9f5e2d40a748e33fde1fe8d4ae
A-Pogam <alicepogam59@hotmail.com>
2023-06-12 19:03:42 +0200

    overlay on the banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -2,8 +2,8 @@ const Banner = ({ imgSrc, alt, text }) => {
   return (
     <div className="banner">
       <div className="banner-container">
+      <div className="gradient-overlay-banner"></div>
         <img className="banner-container-img" src={imgSrc} alt={alt} />
-        <div className="gradient-overlay"></div>
         {text && <div className="banner-container-text">{text}</div>}
       </div>
     </div>
