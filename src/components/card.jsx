e0b9f35826734ced1a1611ed8c373becf3234e47
A-Pogam <alicepogam59@hotmail.com>
2023-06-12 16:47:46 +0200

    improved text and image mobile devise for cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,7 +5,7 @@ const Card = ({ id, title, image, text }) => {
     <div className="card">
       <Link to={`/housing/${id}`}>
         <img src={image} alt={title} className="small-image" />
-        <div className="card-text">{text}</div>
+        <div className="card-text">{title}</div>
       </Link>
     </div>
   );
