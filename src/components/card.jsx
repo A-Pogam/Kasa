c5cce203b4b3fabc4537ab273c1a75a0aa90f1e3
A-Pogam <alicepogam59@hotmail.com>
2023-06-12 17:42:55 +0200

    added background img cards and improved mobile devise cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom';
 const Card = ({ id, title, image, text }) => {
   return (
     <div className="card">
-      <Link to={`/housing/${id}`}>
-        <img src={image} alt={title} className="small-image" />
-        <div className="card-text">{title}</div>
+      <Link to={`/housing/${id}`} className="card-link">
+        <div className="image-container">
+        <div className="gradient-overlay"></div>
+          <img src={image} alt={title} className="small-image" />
+          <div className="card-text">{title}</div>
+        </div>
       </Link>
     </div>
   );
