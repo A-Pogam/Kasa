b925ca6984cacef7db1a8381e58ab00fafbdaca2
A-Pogam <alicepogam59@hotmail.com>
2023-06-13 18:50:47 +0200

    test housing id. profile for mobile devise

--- src/components/housingId.jsx
+++ src/components/housingId.jsx
@@ -1,16 +1,18 @@
 const HousingId = (props) => {
-    return (
-            <div className="id">
-              <div className="id-container">
-                <h1 className="housingId-title">{props.title}</h1>
-                <p className="housingId-location">{props.location}</p>
-              </div>
-              <div className="housingId-profile">
-                <p className="housingId-name">{props.host.name}</p>
-                <img className="housingId-profile-picture" src={props.host.picture} alt={props.host.name} />
-              </div>
-            </div>
-    );
-  };
-  
-  export default HousingId;
+  return (
+    <div className="id">
+      <div className="id-container">
+        <h1 className="housingId-title">{props.title}</h1>
+        <p className="housingId-location">{props.location}</p>
+      </div>
+      <div className="housingId-container">
+        <div className="housingId-profile">
+          <p className="housingId-name">{props.host.name}</p>
+          <img className="housingId-profile-picture" src={props.host.picture} alt={props.host.name} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HousingId;
