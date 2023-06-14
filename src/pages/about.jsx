9e2b4943e8bc6531639607cc2c4a937cec9136a8
A-Pogam <alicepogam59@hotmail.com>
2023-06-14 20:48:52 +0200

    created and finished about page scss

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,13 +1,18 @@
+import Banner from "../components/banner";
+import aboutBannerImg from "../assets/bannerAbout.webp";
 import Collapse from "../components/Collapse";  
 import '../styles/pages/about.scss';
 
 const CollapseAbout = () => {
   return (
-    <div>
-      <Collapse title="Fiabilité" />
-      <Collapse title="Respect" />
-      <Collapse title="Service" />
-      <Collapse title="Sécurité" />
+    <div className="home-about">
+      <Banner imgSrc={aboutBannerImg} alt="Bannière Accueil" />
+      <div className="collapse-about">
+        <Collapse title="Fiabilité" />
+        <Collapse title="Respect" />
+        <Collapse title="Service" />
+        <Collapse title="Sécurité" />
+      </div>
     </div>
   );
 };
