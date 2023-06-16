2482dc9bc0bf2a5d1486ec46c927facc767f6f30
A-Pogam <alicepogam59@hotmail.com>
2023-06-16 12:57:50 +0200

    tried to appear the text in about collapse

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,17 +1,21 @@
 import Banner from "../components/banner";
 import aboutBannerImg from "../assets/bannerAbout.webp";
-import Collapse from "../components/Collapse";  
+import Collapse from "../components/Collapse";
 import '../styles/pages/about.scss';
+import aboutContent from "../data/aboutContent.json";
 
 const CollapseAbout = () => {
   return (
     <div className="home-about">
       <Banner imgSrc={aboutBannerImg} alt="Bannière Accueil" />
       <div className="collapse-about">
-        <Collapse title="Fiabilité"> <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p> </Collapse>
-        <Collapse title="Respect"> <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p> </Collapse>
-        <Collapse title="Service"> <p>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.</p> </Collapse>
-        <Collapse title="Sécurité"> <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p> </Collapse>
+        {aboutContent.map(({ title, text }) => (
+          <Collapse title={title} key={title}>
+            <div className="collapse-content">
+              <p>{text}</p>
+            </div>
+          </Collapse>
+        ))}
       </div>
     </div>
   );
